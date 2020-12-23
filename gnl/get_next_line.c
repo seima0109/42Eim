49b1b3f0c0a65e19ea0a02274629377ce6be97d2
友納聖真 <stomonoh@tomonoukiyoshishinnoMacBook-Pro.local>
2020-12-23 23:57:57 +0900

    0 leaks

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,25 +6,17 @@
 /*   By: stomonoh <stomonoh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/07 14:16:16 by stomonoh          #+#    #+#             */
-/*   Updated: 2020/12/23 21:35:46 by stomonoh         ###   ########.fr       */
+/*   Updated: 2020/12/23 23:55:43 by stomonoh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-/*
-int	ret_error(char *buf, char *save)
-{
-	free(buf);
-	free(save);
-	return (-1);
-}*/
 
 int	create_line(char **line, char *buf, char **save)
 {
 	char	*tmp;
 	char	*new_line;
 
-//	write(1, "===create_line===\n", 18);
 	if (!(new_line = ft_strchr(buf, '\n')))
 	{
 		tmp = ft_strjoin(*line, buf);
@@ -33,14 +25,11 @@ int	create_line(char **line, char *buf, char **save)
 		return (0);
 	}
 	*new_line = '\0';
-//	write(1, "===after newline===\n", 20);
 	if (!(tmp = ft_strjoin(*line, buf)))
 		return (-1);
-//	printf("before line free\n");
 	FREE_AND_NULL(line);
 	*line = tmp;
-	//printf("before tmp_2 free\n");
-	//FREE_AND_NULL(&tmp);
+	FREE_AND_NULL(save);
 	if (!(*save = ft_strdup(new_line + 1)))
 		return (-1);
 	return (1);
@@ -52,32 +41,24 @@ int	read_buf(int fd, char **line, char **save)
 	char	*buf;
 	int		res;
 
-//	write(1, "===read buf===\n", 15);
 	buf = malloc(BUFFER_SIZE + 1);
 	while ((cnt = read(fd, buf, BUFFER_SIZE)))
 	{
 		if (cnt == -1)
 		{
 			FREE_AND_NULL(&buf);
-			FREE_AND_NULL(line);//エラー専用の関数作りたいかも
+			FREE_AND_NULL(line);
+			FREE_AND_NULL(save);
 			return (-1);
 		}
-//		write(1, "before buf[cnt] = 0\n", 20);
 		buf[cnt] = '\0';
-//		write(1, "after buf[cnt] = 0\n", 20);
 		if ((res = create_line(line, buf, save)) != 0)
 		{
 			FREE_AND_NULL(&buf);
 			return (res);
 		}
-		//EOFでも改行入りでもない場合
-		/*tmp = ft_strjoin(*line, buf);
-		FREE_AND_NULL(line);
-		*line = tmp;*/
 	}
-//	printf("cnt = [%d]", cnt);
 	buf[cnt] = '\0';
-//	printf("before save(read_buf) free\n");
 	FREE_AND_NULL(save);
 	FREE_AND_NULL(&buf);
 	return (0);
@@ -85,23 +66,25 @@ int	read_buf(int fd, char **line, char **save)
 
 int	check_save(char **save, char **line)
 {
-	char	*tmp;
+	char	*tmp;//
 	char	*new_line;
 
-//	write(1, "===check save===", 16);
 	tmp = ft_strdup(*save);
 	if ((new_line = ft_strchr(tmp, '\n')))
 	{
 		*new_line = '\0';
+		FREE_AND_NULL(line);//
 		*line = ft_strdup(tmp);
-		FREE_AND_NULL(save);
+		FREE_AND_NULL(save);//
 		*save = ft_strdup(new_line + 1);
+		FREE_AND_NULL(&tmp);//
 		return (1);
 	}
+	FREE_AND_NULL(line);//
 	if (!(*line = ft_strdup(*save)))
 		return (-1);
-//	printf("before save(check save) free\n");
-	FREE_AND_NULL(save);
+	FREE_AND_NULL(save);//
+	FREE_AND_NULL(&tmp);//
 	return (0);
 }
 
@@ -110,38 +93,18 @@ int	get_next_line(int fd, char **line)
 	static char	*save;
 	int			ret;
 
-//	printf("in gnl\n");
 	if ((BUFFER_SIZE <= 0) || fd < 0 || 255 < fd 
-	|| !line || !(*line = ft_strdup("")))
+	|| !line || !(*line = ft_strdup("")))//
 		return (-1);
 	ret = 0;
 	if (!save)
 	{
-		if (!(save =  ft_strdup("")))
+		if (!(save =  ft_strdup("")))//
 			return (-1);
 	}
 	else
-		ret = check_save(&save, line);//1ならリターン0ならread
+		ret = check_save(&save, line);
 	if (!ret)
-	{
 		ret = read_buf(fd, line, &save);
-	}
 	return (ret);
 }
-	/*
-	while ((ret = read(fd, buf, BUFFER_SIZE)))
-	{
-		buf[BUFFER_SIZE] = '\0';
-		if (ret = -1)
-			return(ret_error(buf, save));
-		if (!save)//1周目またはsaveが空の時
-			if (!(save = ft_strdup("")))//セグフォケア
-				return (ret_error);
-		if (!(tmp = ft_strjoin(save, buf)))
-			return (ret_error);
-		free(save);
-		save = tmp;
-	}
-	return (test(save, line));
-	return (1);
-}*/
